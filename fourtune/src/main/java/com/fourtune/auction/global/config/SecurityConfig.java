9a2b21a38f0fa92c283e451a0332aa804b11513a
daeyeon <kdy4410121@naver.com>
2026-01-14 11:23:52 +0900

    feat: jwtFilter import문 추가 (#25)

--- fourtune/src/main/java/com/fourtune/auction/global/config/SecurityConfig.java
+++ fourtune/src/main/java/com/fourtune/auction/global/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.fourtune.auction.global.config;
 
+import com.fourtune.auction.global.security.jwt.JwtAuthenticationFilter;
 import com.fourtune.auction.global.security.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
