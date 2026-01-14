98876e97387be3f6c4b71fbcc2b467dc98de317d
daeyeon <kdy4410121@naver.com>
2026-01-14 12:17:59 +0900

    refactor: users/login 주석 (#25)

--- fourtune/src/main/java/com/fourtune/auction/boundedContext/user/adapter/in/web/UserController.java
+++ fourtune/src/main/java/com/fourtune/auction/boundedContext/user/adapter/in/web/UserController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -21,37 +22,41 @@ public class UserController {
         userFacade.signup(request);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
-
+    /*
     @PostMapping("/login")
     public ResponseEntity<UserLoginResponse> login(@RequestBody @Valid UserLoginRequest request) {
         UserLoginResponse response = userFacade.login(request);
         return ResponseEntity.ok(response);
     }
-
+    */
     @PatchMapping("/profile")
     public ResponseEntity<Void> updateProfile(
-            @AuthenticationPrincipal Long userId,
+            @AuthenticationPrincipal UserDetails user,
             @RequestBody @Valid UserUpdateRequest request
     ) {
+        Long userId = Long.parseLong(user.getUsername());
         userFacade.updateProfile(userId, request);
         return ResponseEntity.ok().build();
     }
 
     @PatchMapping("/password")
     public ResponseEntity<Void> changePassword(
-            @AuthenticationPrincipal Long userId,
+            @AuthenticationPrincipal UserDetails user,
             @RequestBody @Valid UserPasswordChangeRequest request
     ) {
+        Long userId = Long.parseLong(user.getUsername());
         userFacade.changePassword(userId, request);
         return ResponseEntity.ok().build();
     }
 
     @DeleteMapping("/withdraw")
     public ResponseEntity<Void> withdraw(
-            @AuthenticationPrincipal Long userId,
+            @AuthenticationPrincipal UserDetails user,
             @RequestBody @Valid UserWithdrawRequest request
     ) {
+        Long userId = Long.parseLong(user.getUsername());
         userFacade.withdraw(userId, request);
         return ResponseEntity.ok().build();
     }
+
 }
