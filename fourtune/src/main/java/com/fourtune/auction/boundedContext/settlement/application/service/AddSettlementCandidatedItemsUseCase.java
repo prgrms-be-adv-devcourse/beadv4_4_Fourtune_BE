fb2be17c6d31537f2db52914d0b7395054f7779a
jisoo615 <57720285+jisoo615@users.noreply.github.com>
2026-01-18 20:18:38 +0900

    comment: todo 작성 (#54)

--- fourtune/src/main/java/com/fourtune/auction/boundedContext/settlement/application/service/AddSettlementCandidatedItemsUseCase.java
+++ fourtune/src/main/java/com/fourtune/auction/boundedContext/settlement/application/service/AddSettlementCandidatedItemsUseCase.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 @Service
 public class AddSettlementCandidatedItemsUseCase {
-    //todo: 결제 완료시 주문서의 아이템을 보고 정산 후보를 저장하기
+    //todo: 결제 완료시 주문서(구매자, 판매자, orderId, amount/price->수수료,대금, paymentDate )의 아이템을 보고 정산 후보를 저장하기
 
 
 }
--- fourtune/src/main/java/com/fourtune/auction/shared/settlement/event/SettlementCompletedEvent.java
+++ fourtune/src/main/java/com/fourtune/auction/shared/settlement/event/SettlementCompletedEvent.java
@@ -7,5 +7,10 @@ import lombok.Data;
 @AllArgsConstructor
 @Data
 public class SettlementCompletedEvent {
+    /**
+     * 리스너 TODO
+     * settlement -> 새 settlement생성 [v]
+     * payment -> cash log 생성 [ ]
+     */
     SettlementDto settlementDto;
 }
