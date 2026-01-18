dc0a12f8ed5235a1c929a9269e6a2d67ebfe8126
jisoo615 <57720285+jisoo615@users.noreply.github.com>
2026-01-18 15:14:56 +0900

    feat: 정산 UseCase, 이벤트 리스너 기본 구조 생성 (#54)

--- fourtune/src/main/java/com/fourtune/auction/boundedContext/settlement/adapter/in/event/SettlementListener.java
+++ fourtune/src/main/java/com/fourtune/auction/boundedContext/settlement/adapter/in/event/SettlementListener.java
@@ -4,4 +4,9 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class SettlementListener {
+    //todo: member created, modified -> user sync, create settlement
+
+    //todo: payment completed or order completed event -> 정산 후보로 등록
+
+    //todo: settlement completed event -> create payee's new empty settlement
 }
---
+++ fourtune/src/main/java/com/fourtune/auction/boundedContext/settlement/application/service/CompleteSettlementChunkUseCase.java
@@ -0,0 +1,7 @@
+package com.fourtune.auction.boundedContext.settlement.application.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CompleteSettlementChunkUseCase {
+}
