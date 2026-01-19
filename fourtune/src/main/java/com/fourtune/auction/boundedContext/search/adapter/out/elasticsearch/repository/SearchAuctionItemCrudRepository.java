935f22f5b4302602e8132a21ae27354494a515c5
myosookim <o_rul8_2@naver.com>
2026-01-19 09:11:45 +0900

    revert: test 환경에서 Elasticsearch 관련 Bean 비활성화 취소
    This reverts commit 2868ed8c7607fcb44f543d3b04daf6d8ae21994b.

--- fourtune/src/main/java/com/fourtune/auction/boundedContext/search/adapter/out/elasticsearch/repository/SearchAuctionItemCrudRepository.java
+++ fourtune/src/main/java/com/fourtune/auction/boundedContext/search/adapter/out/elasticsearch/repository/SearchAuctionItemCrudRepository.java
@@ -1,9 +1,7 @@
 package com.fourtune.auction.boundedContext.search.adapter.out.elasticsearch.repository;
 
 import com.fourtune.auction.boundedContext.search.adapter.out.elasticsearch.document.SearchAuctionItemDocument;
-import org.springframework.context.annotation.Profile;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
-@Profile("!test")
 public interface SearchAuctionItemCrudRepository extends ElasticsearchRepository<SearchAuctionItemDocument, Long> {
 }
