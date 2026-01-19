1d64ebcdcaa81d07b3e8ea75e52462e2b9e0152e
daeyeon <kdy4410121@naver.com>
2026-01-19 11:40:07 +0900

    feat: 관심상품 db 저장, 존재여부 메서드 추가 (#67)

--- fourtune/src/main/java/com/fourtune/auction/boundedContext/watchList/application/service/WatchListSupport.java
+++ fourtune/src/main/java/com/fourtune/auction/boundedContext/watchList/application/service/WatchListSupport.java
@@ -1,5 +1,6 @@
 package com.fourtune.auction.boundedContext.watchList.application.service;
 
+import com.fourtune.auction.boundedContext.watchList.domain.WatchList;
 import com.fourtune.auction.boundedContext.watchList.domain.WatchListAuctionItem;
 import com.fourtune.auction.boundedContext.watchList.domain.WatchListUser;
 import com.fourtune.auction.boundedContext.watchList.port.out.WatchListItemsRepository;
@@ -7,6 +8,8 @@ import com.fourtune.auction.boundedContext.watchList.port.out.WatchListRepositor
 import com.fourtune.auction.boundedContext.watchList.port.out.WatchListUserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -16,8 +19,12 @@ public class WatchListSupport {
     private final WatchListUserRepository watchListUserRepository;
     private final WatchListRepository watchListRepository;
 
+    public boolean existsByUserIdAndAuctionItemId(Long userId, Long auctionId){
+        return watchListRepository.existsByUserIdAndAuctionItemId(userId, auctionId);
+    }
+
     public void deleteByUserIdAndAuctionItemId(Long userId, Long auctionId){
-        deleteByUserIdAndAuctionItemId(userId, auctionId);
+        watchListRepository.deleteByUserIdAndAuctionItemId(userId, auctionId);
     }
 
     public WatchListUser findByUserId(Long userId){
@@ -28,6 +35,14 @@ public class WatchListSupport {
     public WatchListAuctionItem findByAuctionItemId(Long auctionId){
         return watchListItemsRepository.findById(auctionId)
                 .orElseThrow(() -> new IllegalArgumentException("동기화된 유저 정보가 없습니다."));
+    }
+
+    public WatchList save(WatchList watchList){
+        return watchListRepository.save(watchList);
+    }
+
+    public List<WatchList> findAllByUserId(Long userId){
+        return watchListRepository.findAllByUserId(userId);
     }
 
 }
