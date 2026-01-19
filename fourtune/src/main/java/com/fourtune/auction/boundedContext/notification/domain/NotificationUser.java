39329bae92b8fff260249e380be7b71d59a2fad5
daeyeon <kdy4410121@naver.com>
2026-01-19 18:08:42 +0900

    refactor: 빌더패턴으로 변경, sync 함수 생성(#87)

--- fourtune/src/main/java/com/fourtune/auction/boundedContext/notification/domain/NotificationUser.java
+++ fourtune/src/main/java/com/fourtune/auction/boundedContext/notification/domain/NotificationUser.java
@@ -3,17 +3,19 @@ package com.fourtune.auction.boundedContext.notification.domain;
 import com.fourtune.auction.shared.user.domain.ReplicaUser;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "Notification_user")
+@Table(name = "notification_users")
 @Getter
 @NoArgsConstructor
 public class NotificationUser extends ReplicaUser {
 
+    @Builder
     public NotificationUser(
             Long id,
             String email,
@@ -25,6 +27,10 @@ public class NotificationUser extends ReplicaUser {
             LocalDateTime deletedAt
     ) {
         super(id, email, nickname, password, phoneNumber, createdAt, updatedAt, deletedAt);
+    }
+
+    public void syncProfile(String nickname, String email) {
+        super.updateInfo(nickname, email);
     }
 
 }
