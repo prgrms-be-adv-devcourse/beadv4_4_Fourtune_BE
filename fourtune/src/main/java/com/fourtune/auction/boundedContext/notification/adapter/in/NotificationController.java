b213f39f7a1c7184eb208119a3ba69454df75512
daeyeon <kdy4410121@naver.com>
2026-01-19 16:05:16 +0900

    feat: 알림 컨트롤러 생성 (#83)

---
+++ fourtune/src/main/java/com/fourtune/auction/boundedContext/notification/adapter/in/NotificationController.java
@@ -0,0 +1,55 @@
+package com.fourtune.auction.boundedContext.notification.adapter.in;
+
+import com.fourtune.auction.boundedContext.notification.application.NotificationFacade;
+import com.fourtune.auction.shared.auth.dto.UserContext;
+import com.fourtune.auction.shared.notification.dto.NotificationResponseDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/notifications")
+public class NotificationController {
+
+    private final NotificationFacade notificationFacade;
+
+    @GetMapping
+    public ResponseEntity<List<NotificationResponseDto>> getMyNotifications(
+            @AuthenticationPrincipal UserContext userContext
+            ) {
+        log.info("GET /api/v1/notifications - User: {}", userContext.id());
+
+        List<NotificationResponseDto> response = notificationFacade.getMyNotifications(userContext.id());
+
+        return ResponseEntity.ok(response);
+    }
+
+    @PatchMapping("{notificationId}/read")
+    public ResponseEntity<Void> markAsRead(
+            @AuthenticationPrincipal UserContext userContext,
+            @PathVariable Long notificationId
+    ) {
+        log.info("PATCH /api/v1/notifications/{}/read - User: {}", notificationId, userContext.id());
+
+        notificationFacade.markAsRead(userContext.id(), notificationId);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{notificationId}")
+    public ResponseEntity<Void> deleteNotification(
+            @AuthenticationPrincipal UserContext userContext,
+            @RequestBody Long notificationId
+    ) {
+        log.info("DELETE /api/v1/notifications/{} - User: {}", notificationId, userContext.id());
+
+        notificationFacade.deleteNotification(userContext.id(), notificationId);
+        return ResponseEntity.noContent().build();
+    }
+
+}
