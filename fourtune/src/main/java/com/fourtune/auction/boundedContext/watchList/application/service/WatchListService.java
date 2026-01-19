c9c421b81b21fb84b0816c28569969ad3068c2c7
daeyeon <kdy4410121@naver.com>
2026-01-19 11:41:44 +0900

    feat: 관심상품 등록, 제거, 조회 메서드 생성 (#67)

---
+++ fourtune/src/main/java/com/fourtune/auction/boundedContext/watchList/application/service/WatchListService.java
@@ -0,0 +1,53 @@
+package com.fourtune.auction.boundedContext.watchList.application.service;
+
+import com.fourtune.auction.boundedContext.watchList.domain.WatchList;
+import com.fourtune.auction.boundedContext.watchList.domain.WatchListAuctionItem;
+import com.fourtune.auction.boundedContext.watchList.domain.WatchListUser;
+import com.fourtune.auction.shared.watchList.dto.WatchListResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class WatchListService {
+
+    private final WatchListSupport watchListSupport;
+
+
+    @Transactional
+    public void toggleWatchList(Long userId, Long auctionItemId) {
+        if (isExistWatchList(userId, auctionItemId)) {
+            watchListSupport.deleteByUserIdAndAuctionItemId(userId, auctionItemId);
+        }
+        else {
+            WatchListUser user = watchListSupport.findByUserId(userId);
+            WatchListAuctionItem item = watchListSupport.findByAuctionItemId(auctionItemId);
+
+            WatchList watchList = WatchList.builder()
+                    .user(user)
+                    .auctionItem(item)
+                    .build();
+
+            watchListSupport.save(watchList);
+        }
+    }
+
+    public List<WatchListResponseDto> getMyWatchLists(Long userId) {
+        return watchListSupport.findAllByUserId(userId).stream()
+                .map(WatchListResponseDto::from)
+                .collect(Collectors.toList());
+    }
+
+    private boolean isExistWatchList(Long userId, Long itemId){
+        if (watchListSupport.existsByUserIdAndAuctionItemId(userId, itemId)) {
+            return true;
+        }
+
+        return false;
+    }
+}
