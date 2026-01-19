74b9a43301302744ee5998b4af17209f1ce517d1
daeyeon <kdy4410121@naver.com>
2026-01-19 11:00:17 +0900

    refactor: 의존성 제거를 위해 WatchList 관련 엔티티로 변경 (#67)

--- fourtune/src/main/java/com/fourtune/auction/boundedContext/watchList/domain/WatchList.java
+++ fourtune/src/main/java/com/fourtune/auction/boundedContext/watchList/domain/WatchList.java
@@ -1,7 +1,5 @@
 package com.fourtune.auction.boundedContext.watchList.domain;
 
-import com.fourtune.auction.boundedContext.auction.domain.entity.AuctionItem;
-import com.fourtune.auction.boundedContext.user.domain.entity.User;
 import com.fourtune.auction.global.common.BaseTimeEntity;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
@@ -11,7 +9,6 @@ import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
-@Table(name = "watch_list")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class WatchList extends BaseTimeEntity {
 
@@ -20,18 +17,18 @@ public class WatchList extends BaseTimeEntity {
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id", nullable = false)
-    private User user;
+    @JoinColumn(name = "user_id")
+    private WatchListUser user;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "auction_item_id", nullable = false)
-    private AuctionItem auctionItem;
+    @JoinColumn(name = "auction_item_id")
+    private WatchListAuctionItem auctionItem;
 
     private boolean isStartAlertSent;
     private boolean isEndAlertSent;
 
     @Builder
-    public WatchList(User user, AuctionItem auctionItem) {
+    public WatchList(WatchListUser user, WatchListAuctionItem auctionItem) {
         this.user = user;
         this.auctionItem = auctionItem;
         this.isStartAlertSent = false;
