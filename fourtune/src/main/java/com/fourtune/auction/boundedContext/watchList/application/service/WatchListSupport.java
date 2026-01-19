6a3d299ae91c789b472290d140c15cb6bfa56542
daeyeon <kdy4410121@naver.com>
2026-01-19 11:04:24 +0900

    feat: JPA 실행하는 support 생성 (#67)

---
+++ fourtune/src/main/java/com/fourtune/auction/boundedContext/watchList/application/service/WatchListSupport.java
@@ -0,0 +1,33 @@
+package com.fourtune.auction.boundedContext.watchList.application.service;
+
+import com.fourtune.auction.boundedContext.watchList.domain.WatchListAuctionItem;
+import com.fourtune.auction.boundedContext.watchList.domain.WatchListUser;
+import com.fourtune.auction.boundedContext.watchList.port.out.WatchListItemsRepository;
+import com.fourtune.auction.boundedContext.watchList.port.out.WatchListRepository;
+import com.fourtune.auction.boundedContext.watchList.port.out.WatchListUserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class WatchListSupport {
+
+    private final WatchListItemsRepository watchListItemsRepository;
+    private final WatchListUserRepository watchListUserRepository;
+    private final WatchListRepository watchListRepository;
+
+    public void deleteByUserIdAndAuctionItemId(Long userId, Long auctionId){
+        deleteByUserIdAndAuctionItemId(userId, auctionId);
+    }
+
+    public WatchListUser findByUserId(Long userId){
+        return watchListUserRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("동기화된 유저 정보가 없습니다."));
+    }
+
+    public WatchListAuctionItem findByAuctionItemId(Long auctionId){
+        return watchListItemsRepository.findById(auctionId)
+                .orElseThrow(() -> new IllegalArgumentException("동기화된 유저 정보가 없습니다."));
+    }
+
+}
