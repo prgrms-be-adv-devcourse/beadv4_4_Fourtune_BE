fa4e30dbd1678fd58d14a86a9e27798d65624431
daeyeon <kdy4410121@naver.com>
2026-01-19 16:46:09 +0900

    feat: 알림 타입 enum 추가 (#87)

--- fourtune/src/main/java/com/fourtune/auction/boundedContext/notification/domain/constant/NotificationType.java
+++ fourtune/src/main/java/com/fourtune/auction/boundedContext/notification/domain/constant/NotificationType.java
@@ -4,12 +4,27 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
-@RequiredArgsConstructor
 public enum NotificationType {
-    BID("입찰 알림"),
-    WON("낙찰 알림"),
-    PAYMENT("결제 알림"),
-    INTEREST("관심상품 알림");
+    OUTBID("상위 입찰자가 발생했습니다!", "'%s' 상품의 가격이 %s원으로 갱신되었습니다. 다시 입찰에 도전하세요!"),
+    AUCTION_SUCCESS("낙찰 성공!", "'%s' 상품을 %s원에 낙찰받았습니다. 축하합니다!"),
+    AUCTION_FAILED("아쉬운 패찰", "'%s' 상품 낙찰에 실패했습니다."),
+    PAYMENT("결제 알림", "결제 만료일이 1일 남았습니다"),
+    WATCHLIST_START("관심상품 시작 5분 전", "'%s' 상품 경매 시작 5분 전입니다."),
+    WATCHLIST_END("관심상품 종료 5분 전", "'%s' 상품 경매 종료 5분 전입니다.");
 
-    private final String description;
+    private final String titleTemplate;
+    private final String contentTemplate;
+
+    NotificationType(String titleTemplate, String contentTemplate) {
+        this.titleTemplate = titleTemplate;
+        this.contentTemplate = contentTemplate;
+    }
+
+    public String getTitle() {
+        return titleTemplate;
+    }
+
+    public String makeContent(Object... args) {
+        return String.format(contentTemplate, args);
+    }
 }
