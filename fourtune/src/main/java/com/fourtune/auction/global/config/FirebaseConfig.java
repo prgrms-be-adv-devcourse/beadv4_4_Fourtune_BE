fd4195b02432a95641c3937939f0866a98f63004
daeyeon <kdy4410121@naver.com>
2026-01-22 12:26:49 +0900

    refactor: config 파일 변경 (#116)

--- fourtune/src/main/java/com/fourtune/auction/global/config/FirebaseConfig.java
+++ fourtune/src/main/java/com/fourtune/auction/global/config/FirebaseConfig.java
@@ -4,41 +4,46 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
-import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
+import java.io.IOException;
 import java.io.InputStream;
 
 @Configuration
 @Slf4j
 public class FirebaseConfig {
-
-    @PostConstruct
-    public void initialize() {
-        try {
-            if (!FirebaseApp.getApps().isEmpty()) {
-                return;
-            }
-            InputStream serviceAccount = new ClassPathResource("firebase-service-account.json").getInputStream();
-
-            FirebaseOptions options = FirebaseOptions.builder()
-                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                    .build();
-
-            FirebaseApp.initializeApp(options);
-            log.info("🔥 Firebase(FCM) 연결 성공!");
-
-        } catch (Exception e) {
-            log.error("🚫 Firebase 초기화 실패: json 파일 위치나 내용을 확인하세요.", e);
-        }
-    }
-
     @Bean
     public FirebaseMessaging firebaseMessaging() {
-        return FirebaseMessaging.getInstance();
-    }
+        FirebaseApp firebaseApp = null;
 
+        try {
+            if (!FirebaseApp.getApps().isEmpty()) {
+                firebaseApp = FirebaseApp.getInstance();
+            }
+        } catch (Exception e) {
+        }
+
+        if (firebaseApp == null) {
+            try {
+                InputStream serviceAccount = new ClassPathResource("firebase-service-account.json").getInputStream();
+                FirebaseOptions options = FirebaseOptions.builder()
+                        .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                        .build();
+
+                firebaseApp = FirebaseApp.initializeApp(options);
+                log.info("🔥 Firebase(FCM) 연결 성공!");
+
+            } catch (IllegalStateException e) {
+                firebaseApp = FirebaseApp.getInstance();
+            } catch (IOException e) {
+                log.error("🚫 Firebase 초기화 실패: json 파일 위치나 내용을 확인하세요.", e);
+                throw new RuntimeException(e);
+            }
+        }
+
+        return FirebaseMessaging.getInstance(firebaseApp);
+    }
 }
