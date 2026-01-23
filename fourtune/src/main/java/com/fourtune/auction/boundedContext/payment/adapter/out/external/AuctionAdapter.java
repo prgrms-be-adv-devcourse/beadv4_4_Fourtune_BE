ad4cb358707af1ed10615edb4391988fee64c6d0
jisoo615 <57720285+jisoo615@users.noreply.github.com>
2026-01-23 12:35:23 +0900

    feat: 주문데이터 조회 api (#129)

--- fourtune/src/main/java/com/fourtune/auction/boundedContext/payment/adapter/out/external/AuctionAdapter.java
+++ fourtune/src/main/java/com/fourtune/auction/boundedContext/payment/adapter/out/external/AuctionAdapter.java
@@ -1,7 +1,5 @@
 package com.fourtune.auction.boundedContext.payment.adapter.out.external;
 
-import com.fourtune.auction.boundedContext.auction.domain.constant.OrderStatus;
-import com.fourtune.auction.boundedContext.auction.domain.constant.OrderType;
 import com.fourtune.auction.boundedContext.payment.port.out.AuctionPort;
 import com.fourtune.auction.global.config.WebClientConfig;
 import com.fourtune.auction.global.error.ErrorCode;
@@ -10,12 +8,8 @@ import com.fourtune.auction.shared.auction.dto.OrderDetailResponse;
 import com.fourtune.auction.shared.payment.dto.OrderDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
-
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
-import java.util.List;
 
 @Slf4j
 @Component
@@ -25,43 +19,27 @@ public class AuctionAdapter implements AuctionPort {
     private final WebClientConfig webclient;
 
     // 경매 모듈 API 주소 (application.yml 등에서 관리하는 것이 좋음)
-    private static final String AUCTION_MODULE_URL = "http://localhost:8080/api/internal/auction/orders/";
+    @Value("${api.auction.base-url}")
+    private String BASE_URL;
+    private static final String AUCTION_MODULE_URL =
+            "/api/v1/orders/public/";
 
     @Override
     public OrderDto getOrder(String orderNo) {
         try {
-//            // 예시: GET /api/internal/auction/orders/{orderId}
-//            String url = AUCTION_MODULE_URL + orderNo;
-//
-//            OrderDto orderDto = webclient.webClient()
-//                    .get()
-//                    .uri(url)
-//                    .retrieve()
-//                    .bodyToMono(OrderDto.class)
-//                    .block();
-//
-//            if (orderDto == null) {
-//                throw new BusinessException(ErrorCode.PAYMENT_AUCTION_ORDER_NOT_FOUND);
-//            }
+            String url = BASE_URL + AUCTION_MODULE_URL + orderNo;
 
-//            return orderDto;
-            OrderDetailResponse orderDetailResponse = new OrderDetailResponse(
-                                    1L,                             // id
-                                    "ORDER-TEST-0001",              // orderId
-                                    1L,                             // auctionId
-                                    "테스트 아이템",                  // auctionTitle
-                                    "http://localhost:8080/img",    // thumbnailUrl
-                                    1L,                             // winnerId
-                                    "구매자닉네임",                   // winnerNickname
-                                    2L,                             // sellerId
-                                    "판매자닉네임",                   // sellerNickname
-                                    BigDecimal.valueOf(5000),       // finalPrice
-                                    null,                           // orderType
-                                    OrderStatus.PENDING,          // status
-                                    null,             // paymentKey
-                                    null,            // paidAt
-                                    LocalDateTime.now()             // createdAt
-                            );
+            OrderDetailResponse orderDetailResponse = webclient.webClient()
+                    .get()
+                    .uri(url)
+                    .retrieve()
+                    .bodyToMono(OrderDetailResponse.class)
+                    .block();
+
+            if (orderDetailResponse == null) {
+                throw new BusinessException(ErrorCode.PAYMENT_AUCTION_ORDER_NOT_FOUND);
+            }
+
             return OrderDto.from(orderDetailResponse);
 
 
