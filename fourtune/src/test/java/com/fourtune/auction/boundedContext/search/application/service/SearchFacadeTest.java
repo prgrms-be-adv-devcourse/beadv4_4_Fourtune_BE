3882095d1565ac22857f6a1b87d98d155f0d0f84
myosookim <o_rul8_2@naver.com>
2026-02-04 17:41:01 +0900

    test: 비동기 로그 저장 전환에 따른 테스트 케이스 수정 (save 검증 제거)

--- fourtune/src/test/java/com/fourtune/auction/boundedContext/search/application/service/SearchFacadeTest.java
+++ fourtune/src/test/java/com/fourtune/auction/boundedContext/search/application/service/SearchFacadeTest.java
@@ -3,7 +3,6 @@ package com.fourtune.auction.boundedContext.search.application.service;
 import com.fourtune.auction.boundedContext.search.domain.SearchCondition;
 import com.fourtune.auction.boundedContext.search.domain.SearchResultPage;
 import com.fourtune.auction.boundedContext.search.domain.SearchAuctionItemView;
-import com.fourtune.auction.boundedContext.search.port.out.SearchLogRepository;
 import com.fourtune.auction.shared.search.event.SearchAuctionItemEvent;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -30,14 +29,11 @@ class SearchFacadeTest {
     private SearchQueryUseCase searchQueryUseCase;
 
     @Mock
-    private SearchLogRepository searchLogRepository;
-
-    @Mock
     private ApplicationEventPublisher eventPublisher;
 
     @Test
-    @DisplayName("검색어가 존재할 경우 검색 로그가 저장되고 이벤트가 발행된다.")
-    void search_WithKeyword_ShouldSaveLogAndPublishEvent() {
+    @DisplayName("검색어가 존재할 경우 검색 이벤트가 발행된다.")
+    void search_WithKeyword_ShouldPublishEvent() {
         // given
         Long userId = 1L;
         String keyword = "test keyword";
@@ -55,16 +51,13 @@ class SearchFacadeTest {
         // 1. 검색 유스케이스 호출 검증 (먼저 호출됨)
         verify(searchQueryUseCase, times(1)).search(condition);
 
-        // 2. 로그 저장 검증
-        verify(searchLogRepository, times(1)).save(any());
-
-        // 3. 이벤트 발행 검증
+        // 2. 이벤트 발행 검증
         verify(eventPublisher, times(1)).publishEvent(any(SearchAuctionItemEvent.class));
     }
 
     @Test
-    @DisplayName("검색어가 없을 경우 로그 저장 및 이벤트 발행이 되지 않는다.")
-    void search_WithoutKeyword_ShouldNotSaveLogOrPublishEvent() {
+    @DisplayName("검색어가 없을 경우 이벤트 발행이 되지 않는다.")
+    void search_WithoutKeyword_ShouldNotPublishEvent() {
         // given
         Long userId = 1L;
         String keyword = "";
@@ -82,10 +75,7 @@ class SearchFacadeTest {
         // 1. 검색 유스케이스는 호출되어야 함
         verify(searchQueryUseCase, times(1)).search(condition);
 
-        // 2. 로그 저장 안됨
-        verify(searchLogRepository, times(0)).save(any());
-
-        // 3. 이벤트 발행 안됨
+        // 2. 이벤트 발행 안됨
         verify(eventPublisher, times(0)).publishEvent(any());
     }
 }
