c29a9e00f06c4a5d6a12d58b3df4b48ffbc05961
youngjun-kim92 <whiteblood92@naver.com>
2026-02-12 14:32:24 +0900

    feat: 경매 도메인 Kafka + Outbox 도입 (payload 래퍼 방식)

--- fourtune/src/main/java/com/fourtune/auction/boundedContext/auction/application/service/AuctionCloseUseCase.java
+++ fourtune/src/main/java/com/fourtune/auction/boundedContext/auction/application/service/AuctionCloseUseCase.java
@@ -4,14 +4,18 @@ import com.fourtune.auction.boundedContext.auction.domain.entity.AuctionItem;
 import com.fourtune.auction.boundedContext.auction.domain.entity.Bid;
 import com.fourtune.auction.boundedContext.auction.domain.entity.ItemImage;
 import com.fourtune.auction.boundedContext.user.application.service.UserFacade;
+import com.fourtune.auction.global.config.EventPublishingConfig;
 import com.fourtune.auction.global.eventPublisher.EventPublisher;
+import com.fourtune.auction.global.outbox.service.OutboxService;
 import com.fourtune.auction.shared.auction.event.AuctionClosedEvent;
 import com.fourtune.auction.shared.auction.event.AuctionItemUpdatedEvent;
+import com.fourtune.auction.shared.auction.kafka.AuctionEventType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Map;
 import java.util.Set;
 
 /**
@@ -25,11 +29,15 @@ import java.util.Set;
 @RequiredArgsConstructor
 public class AuctionCloseUseCase {
 
+    private static final String AGGREGATE_TYPE_AUCTION = "Auction";
+
     private final AuctionSupport auctionSupport;
     private final BidSupport bidSupport;
     private final OrderCreateUseCase orderCreateUseCase;
     private final EventPublisher eventPublisher;
     private final UserFacade userFacade;
+    private final EventPublishingConfig eventPublishingConfig;
+    private final OutboxService outboxService;
 
     /**
      * 경매 종료 처리
@@ -69,20 +77,24 @@ public class AuctionCloseUseCase {
             bidSupport.failAllActiveBids(auctionId);
             
             // 이벤트 발행
-            eventPublisher.publish(new AuctionClosedEvent(
+            AuctionClosedEvent closedEvent = new AuctionClosedEvent(
                     auctionId,
                     auctionItem.getTitle(),
                     auctionItem.getSellerId(),
                     winningBid.getBidderId(),
                     winningBid.getBidAmount(),
                     orderId
-            ));
-            
+            );
+            if (eventPublishingConfig.isAuctionEventsKafkaEnabled()) {
+                outboxService.append(AGGREGATE_TYPE_AUCTION, auctionId, AuctionEventType.AUCTION_CLOSED.name(), Map.of("eventType", AuctionEventType.AUCTION_CLOSED.name(), "aggregateId", auctionId, "data", closedEvent));
+            } else {
+                eventPublisher.publish(closedEvent);
+            }
+
             // Search 인덱싱 전용 이벤트 발행 (스냅샷 형태)
             String thumbnailUrl = extractThumbnailUrl(auctionItem);
             String sellerName = userFacade.getNicknamesByIds(Set.of(auctionItem.getSellerId())).getOrDefault(auctionItem.getSellerId(), null);
-
-            eventPublisher.publish(new AuctionItemUpdatedEvent(
+            AuctionItemUpdatedEvent itemUpdatedEvent = new AuctionItemUpdatedEvent(
                     auctionItem.getId(),
                     auctionItem.getSellerId(),
                     sellerName,
@@ -102,26 +114,35 @@ public class AuctionCloseUseCase {
                     auctionItem.getViewCount(),
                     auctionItem.getBidCount(),
                     auctionItem.getWatchlistCount()
-            ));
+            );
+            if (eventPublishingConfig.isAuctionEventsKafkaEnabled()) {
+                outboxService.append(AGGREGATE_TYPE_AUCTION, auctionId, AuctionEventType.AUCTION_ITEM_UPDATED.name(), Map.of("eventType", AuctionEventType.AUCTION_ITEM_UPDATED.name(), "aggregateId", auctionId, "data", itemUpdatedEvent));
+            } else {
+                eventPublisher.publish(itemUpdatedEvent);
+            }
         } else {
             // 4-2. 낙찰자가 없는 경우 (입찰이 없었음)
             auctionItem.close();
-            
+
             // 이벤트 발행
-            eventPublisher.publish(new AuctionClosedEvent(
+            AuctionClosedEvent closedEventNoWinner = new AuctionClosedEvent(
                     auctionId,
                     auctionItem.getTitle(),
                     auctionItem.getSellerId(),
                     null,  // 낙찰자 없음
                     null,  // 낙찰가 없음
                     null   // 주문번호 없음
-            ));
-            
+            );
+            if (eventPublishingConfig.isAuctionEventsKafkaEnabled()) {
+                outboxService.append(AGGREGATE_TYPE_AUCTION, auctionId, AuctionEventType.AUCTION_CLOSED.name(), Map.of("eventType", AuctionEventType.AUCTION_CLOSED.name(), "aggregateId", auctionId, "data", closedEventNoWinner));
+            } else {
+                eventPublisher.publish(closedEventNoWinner);
+            }
+
             // Search 인덱싱 전용 이벤트 발행 (스냅샷 형태)
             String thumbnailUrl = extractThumbnailUrl(auctionItem);
             String sellerName = userFacade.getNicknamesByIds(java.util.Set.of(auctionItem.getSellerId())).getOrDefault(auctionItem.getSellerId(), null);
-
-            eventPublisher.publish(new AuctionItemUpdatedEvent(
+            AuctionItemUpdatedEvent itemUpdatedEventNoWinner = new AuctionItemUpdatedEvent(
                     auctionItem.getId(),
                     auctionItem.getSellerId(),
                     sellerName,
@@ -141,10 +162,15 @@ public class AuctionCloseUseCase {
                     auctionItem.getViewCount(),
                     auctionItem.getBidCount(),
                     auctionItem.getWatchlistCount()
-            ));
+            );
+            if (eventPublishingConfig.isAuctionEventsKafkaEnabled()) {
+                outboxService.append(AGGREGATE_TYPE_AUCTION, auctionId, AuctionEventType.AUCTION_ITEM_UPDATED.name(), Map.of("eventType", AuctionEventType.AUCTION_ITEM_UPDATED.name(), "aggregateId", auctionId, "data", itemUpdatedEventNoWinner));
+            } else {
+                eventPublisher.publish(itemUpdatedEventNoWinner);
+            }
         }
     }
-    
+
     /**
      * 썸네일 URL 추출
      */
