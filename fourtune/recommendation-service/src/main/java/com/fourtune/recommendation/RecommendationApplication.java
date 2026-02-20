f418e685763da8ec757101a32aaac180900d2a07
myosookim <o_rul8_2@naver.com>
2026-02-21 02:37:25 +0900

    chore: common 모듈 분리에 따른 auction, recommend 실행 클래스 애노테이션 설정 수정

--- fourtune/recommendation-service/src/main/java/com/fourtune/recommendation/RecommendationApplication.java
+++ fourtune/recommendation-service/src/main/java/com/fourtune/recommendation/RecommendationApplication.java
@@ -10,36 +10,14 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 
-@SpringBootApplication(scanBasePackages = {
-                "com.fourtune.recommendation",
-                "com.fourtune.core",
-                "com.fourtune.security"
-})
+@SpringBootApplication(scanBasePackages = "com.fourtune")
 @EnableJpaAuditing
 @EnableScheduling
-@EnableFeignClients(basePackages = {
-                "com.fourtune.recommendation",
-                "com.fourtune.core",
-                "com.fourtune.security"
-})
-@EnableJpaRepositories(basePackages = {
-                "com.fourtune.recommendation",
-                "com.fourtune.core",
-                "com.fourtune.security"
-})
-@EntityScan(basePackages = {
-                "com.fourtune.recommendation",
-                "com.fourtune.core",
-                "com.fourtune.security"
-})
+@EnableFeignClients(basePackages = "com.fourtune.recommendation")
+@EnableJpaRepositories(basePackages = "com.fourtune")
+@EntityScan(basePackages = "com.fourtune")
 public class RecommendationApplication {
         public static void main(String[] args) {
                 SpringApplication.run(RecommendationApplication.class, args);
-        }
-
-        // Common 모듈의 SecurityConfig에서 필요한 빈 등록 (런타임 에러 해결용)
-        @Bean
-        public DefaultOAuth2UserService defaultOAuth2UserService() {
-                return new DefaultOAuth2UserService();
         }
 }
