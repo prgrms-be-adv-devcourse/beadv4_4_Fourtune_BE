6cdabc9bc72aeea442712cc5f43eeda85b26b1a1
myosookim <o_rul8_2@naver.com>
2026-02-21 21:12:50 +0900

    chore: 추천 서비스 빌드 의존성(Kafka, Shared) 및 공통 설정 추가

--- fourtune/recommendation-service/src/main/java/com/fourtune/recommendation/RecommendationApplication.java
+++ fourtune/recommendation-service/src/main/java/com/fourtune/recommendation/RecommendationApplication.java
@@ -4,11 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 
 @SpringBootApplication(scanBasePackages = "com.fourtune")
 @EnableJpaAuditing
