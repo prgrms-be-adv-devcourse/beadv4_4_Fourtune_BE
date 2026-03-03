d83cbc6c55844954583814d1a41df491caac3948
myosookim <o_rul8_2@naver.com>
2026-03-04 02:01:09 +0900

    test: 컨텍스트 로드 오류 수정

--- fourtune/fourtune-api/src/test/java/com/fourtune/auction/FourtuneApplicationTests.java
+++ fourtune/fourtune-api/src/test/java/com/fourtune/auction/FourtuneApplicationTests.java
@@ -3,13 +3,24 @@ package com.fourtune.auction;
 import com.fourtune.api.infrastructure.kafka.notification.NotificationKafkaProducer;
 import com.fourtune.api.infrastructure.kafka.search.SearchKafkaProducer;
 import com.fourtune.api.infrastructure.kafka.watchList.WatchListKafkaProducer;
+import com.fourtune.auction.boundedContext.search.adapter.out.elasticsearch.ElasticsearchTestContainer;
 import com.google.firebase.messaging.FirebaseMessaging;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.testcontainers.elasticsearch.ElasticsearchContainer;
 
 @SpringBootTest
 class FourtuneApplicationTests {
+
+	private static final ElasticsearchContainer elasticsearchContainer = ElasticsearchTestContainer.getInstance();
+
+	@DynamicPropertySource
+	static void setProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.elasticsearch.uris", elasticsearchContainer::getHttpHostAddress);
+	}
 
 	@MockitoBean
 	private FirebaseMessaging firebaseMessaging;
